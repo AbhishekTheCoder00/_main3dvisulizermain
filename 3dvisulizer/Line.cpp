5f77e64518568c479470e1d4e815b7afb82e1b8a
Abhishek Suryawanshi <90126357+AbhishekTheCoder00@users.noreply.github.com>
2024-10-02 13:05:18 +0530

    Update Line.cpp

--- 3dvisulizer/Line.cpp
+++ 3dvisulizer/Line.cpp
@@ -1,10 +1,9 @@
 #include "Line.h"
 
-Line::Line(const Point& start, const Point& end) : start(start), end(end) {}
+Line::Line(double x1, double y1, double x2, double y2) 
+    : x1(x1), y1(y1), x2(x2), y2(y2) {}
 
-// Writing line coordinates to file
 void Line::writeToFile(std::ofstream& file) const {
-    start.writeToFile(file);  // Write start point
-    end.writeToFile(file);    // Write end point
-    file << "\n";  // Separate lines in the file
+    file << x1 << " " << y1 << "\n"  // Starting point
+         << x2 << " " << y2 << "\n"; // Ending point
 }
